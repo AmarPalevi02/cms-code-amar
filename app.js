4554b5bca90e38cdbcaaab536a430758a1174a55
amarNrddn <amarnrddn02@gmail.com>
2024-12-19 13:21:54 +0700

    update

--- app.js
+++ app.js
@@ -16,22 +16,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
 // Routes
-const routeMyJobs = require('./app/api/my_jobs/route');
-const routeBlog = require('./app/api/blog/route');
-const routeProject = require('./app/api/projects/route');
-const routeFeature = require('./app/api/feature/route');
-const routeTechstack = require('./app/api/techstack/route');
+// const routeMyJobs = require('./app/api/my_jobs/route');
+// const routeBlog = require('./app/api/blog/route');
+// const routeProject = require('./app/api/projects/route');
+// const routeFeature = require('./app/api/feature/route');
+// const routeTechstack = require('./app/api/techstack/route');
 
 app.get('/', (req, res) => {
    res.send('Welcome here');
 });
 
-const v1 = '/code-amar/v1';
-app.use(`${v1}/`, routeMyJobs);
-app.use(`${v1}/`, routeBlog);
-app.use(`${v1}/`, routeProject);
-app.use(`${v1}/`, routeFeature);
-app.use(`${v1}/`, routeTechstack);
+// const v1 = '/code-amar/v1';
+// app.use(`${v1}/`, routeMyJobs);
+// app.use(`${v1}/`, routeBlog);
+// app.use(`${v1}/`, routeProject);
+// app.use(`${v1}/`, routeFeature);
+// app.use(`${v1}/`, routeTechstack);
 
 // Ekspor untuk mode server dan AWS Lambda
 module.exports = app;
