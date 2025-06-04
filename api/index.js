10738be1fd987216d4781ba9432a46e191bfcbd9
amarNrddn <amarnrddn02@gmail.com>
2025-06-05 01:23:23 +0700

    update

---
+++ api/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require("cors");
+const path = require('path');
+const logger = require('morgan');
+const compression = require('compression');
+const app = express();
+
+// Middleware
+app.use(compression());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+
+// Routes
+const routeMyJobs = require('../app/api/my_jobs/route');
+const routeBlog = require('../app/api/blog/route');
+const routeProject = require('../app/api/projects/route');
+const routeFeature = require('../app/api/feature/route');
+const routeTechstack = require('../app/api/techstack/route');
+
+app.get('/', (req, res) => {
+   res.send('Welcome here');
+});
+
+const v1 = '/code-amar/v1';
+app.use(`${v1}/`, routeMyJobs);
+app.use(`${v1}/`, routeBlog);
+app.use(`${v1}/`, routeProject);
+app.use(`${v1}/`, routeFeature);
+app.use(`${v1}/`, routeTechstack);
+
+// Ekspor handler untuk Vercel
+const serverless = require('serverless-http');
+module.exports = app;
+module.exports.handler = serverless(app);
+
